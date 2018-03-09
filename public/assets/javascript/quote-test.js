a7aded325ea6247f7520c1a974b567230809368d
Apollo <jackson.harris3@gmail.com>
2018-03-08 20:44:51 -0800

    add firebase

---
+++ public/assets/javascript/quote-test.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+console.log('we ready')
+    // Initialize Firebase
+
+    var config = {
+        apiKey: "AIzaSyCKWMCNkLOish_Bv8jc9ByMO3mVTGnzdSs",
+    authDomain: "level-up-8ab46.firebaseapp.com",
+    databaseURL: "https://level-up-8ab46.firebaseio.com",
+    projectId: "level-up-8ab46",
+    storageBucket: "",
+    messagingSenderId: "959253299439"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+    var provider = new firebase.auth.FacebookAuthProvider();
+    
+    function fbLogin() {
+        // firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+
+    }    
+
+
+    function testing() {
+        console.log('facebook')
+    }
+
+    $('body').on('click', '#fbLogin', fbLogin)    
+})
